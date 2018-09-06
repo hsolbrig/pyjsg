b6224e9534ef6925b1b6e2c1521ac7ab7e0a79d4
hsolbrig <solbrig@jhu.edu>
2018-09-06 13:11:10 -0500

    Up to 0.8b4 to keep pypi in sync

--- pyjsg/parser_impl/__init__.py
+++ pyjsg/parser_impl/__init__.py
@@ -6,7 +6,7 @@ specified by {JSON Schema Grammar<http://github.com/ericprud/jsglib>}
 
 """
 
-__version__ = '0.8b3'
+__version__ = '0.8b4'
 __url__ = 'http://github.com/hsolbrig/pyjsg'
 __license__ = 'Apache 2.0'
 
